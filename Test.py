8236f277e0cf655007bb2aaa7277f481ada5487d
Ricardooddone <85978752+Ricardooddone@users.noreply.github.com>
2021-10-14 15:30:14 -0300

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,70 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "<center>\n",
+    "    <img src=\"https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/Logos/organization_logo/organization_logo.png\" width=\"300\" alt=\"cognitiveclass.ai logo\"  />\n",
+    "</center>\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "#### Add your code below following the instructions given in the course\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "https://www.cognitiveclass.ai"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "| Tables        | Are           | Cool  |\n",
+    "| ------------- |:-------------:| -----:|\n",
+    "| col 3 is      | right-aligned | $1600 |\n",
+    "| col 2 is      | centered      |   $12 |\n",
+    "| zebra stripes | are neat      |    $1 |"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "| Champs        | KDA´s         | Wins  |\n",
+    "| ------------- |:-------------:| -----:|\n",
+    "| morgana       | 10/2/8        | 5     |\n",
+    "| Brand         | 15/4/23       | 10    |\n",
+    "| Lux           | 8/7/15        | 4     |"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python",
+   "language": "python",
+   "name": "conda-env-python-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
